d1e5a77a34aafe58b46bf42145ecfaa21658fcf9
Justin Singh <justin.singh125125@gmail.com>
2019-08-24 21:32:24 -0700

    working on updaing

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,7 +7,22 @@ const Mutations = {
         }, info);
 
         return item;
-    }
+    },
+
+    updateItem(parent, args, ctx, info) {
+        //first take a copy of the updates
+        const updates = { ...args };
+        //remove the id from the updates
+        delete updates.id;
+        //run the update method, this is found in generated prisma 
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            }
+        }, info);
+    },
+
 };
 
 module.exports = Mutations;
