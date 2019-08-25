e984b984de560f9fa38b8c2998254d614d60c4e4
Justin Singh <justin.singh125125@gmail.com>
2019-08-24 19:11:41 -0700

    added uploading photo

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { Mutation } from "react-apollo";
-import Router from "next/router";
-import gql from "graphql-tag";
-import Form from "./styles/Form";
-import formatMoney from "../lib/formatMoney";
-import Error from "./ErrorMessage";
-
-
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -28,9 +26,7 @@ const CREATE_ITEM_MUTATION = gql`
   }
 `;
 
-
 class CreateItem extends Component {
-
     state = {
         title: '',
         description: '',
@@ -38,32 +34,67 @@ class CreateItem extends Component {
         largeImage: '',
         price: 0,
     };
-
     handleChange = e => {
         const { name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name]: val });
     };
+
+    uploadFile = async e => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/dsce5oohx/image/upload', {
+            method: 'POST',
+            body: data,
+        });
+        const file = await res.json();
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url,
+        });
+    };
     render() {
         return (
-
             <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
                 {(createItem, { loading, error }) => (
-                    <Form onSubmit={async (e) => {
-                        e.preventDefault();
-                        //call the mutation
-                        const res = await createItem();
-                        Router.push({
-                            pathname: "/item",
-                            query: { id: res.data.createItem.id }
-                        })
-                    }}>
-
+                    <Form
+                        data-test="form"
+                        onSubmit={async e => {
+                            // Stop the form from submitting
+                            e.preventDefault();
+                            // call the mutation
+                            const res = await createItem();
+                            // change them to the single item page
+                            console.log(res);
+                            Router.push({
+                                pathname: '/items',
+                                query: { id: res.data.createItem.id },
+                            });
+                        }}
+                    >
                         <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor="file">
+                                Image
+                <input
+                                    type="file"
+                                    id="file"
+                                    name="file"
+                                    placeholder="Upload an image"
+                                    required
+                                    onChange={this.uploadFile}
+                                />
+                                {this.state.image && (
+                                    <img width="200" src={this.state.image} alt="Upload Preview" />
+                                )}
+                            </label>
+
                             <label htmlFor="title">
                                 Title
-                            <input
+                <input
                                     type="text"
                                     id="title"
                                     name="title"
@@ -76,7 +107,7 @@ class CreateItem extends Component {
 
                             <label htmlFor="price">
                                 Price
-                            <input
+                <input
                                     type="number"
                                     id="price"
                                     name="price"
@@ -89,7 +120,7 @@ class CreateItem extends Component {
 
                             <label htmlFor="description">
                                 Description
-                             <textarea
+                <textarea
                                     id="description"
                                     name="description"
                                     placeholder="Enter A Description"
